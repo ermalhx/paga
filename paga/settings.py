e1b995cea511d63353b534f4909b93a75fd81dee
Ermalhx <ermalhx@gmail.com>
2022-10-20 20:45:26 +0200

    Update settings.py

--- paga/settings.py
+++ paga/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-b41d.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-1501.up.railway.app/']
 # Application definition
 
 INSTALLED_APPS = [
